a9467df12a35257c80c2ecacd844deb6babf2a97
tusharmalpani20 <tusharmalpani20@gmail.com>
2024-12-11 18:42:37 +0530

    Added employee.js filter

--- custom_app_api/hooks.py
+++ custom_app_api/hooks.py
@@ -45,7 +45,7 @@ app_license = "mit"
 # include js in doctype views
 doctype_js = {
 	# "doctype" : "public/js/doctype.js"
-	"Employee" : "custom_api/doctype/Employee/employee.js"
+	"Employee" : "public/js/employee.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
