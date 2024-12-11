a7568101ad2d3046a82cf3707cf51136034c4ae9
tusharmalpani20 <tusharmalpani20@gmail.com>
2024-12-11 17:13:11 +0530

    Added doctypes

---
+++ custom_app_api/custom_api/doctype/delivery_mapping/delivery_mapping.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Hopnet and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Delivery Mapping", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ custom_app_api/custom_api/doctype/delivery_records/test_delivery_records.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Hopnet and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDeliveryRecords(FrappeTestCase):
+	pass
