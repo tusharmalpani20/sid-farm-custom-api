fba715c4cd634b0e5a9d638f1f925e03fbe099de
tusharmalpani20 <tusharmalpani20@gmail.com>
2024-12-11 18:17:35 +0530

    updated hooks.py to add point,area and zone

--- custom_app_api/hooks.py
+++ custom_app_api/hooks.py
@@ -122,6 +122,8 @@ permission_query_conditions = {
 	"Employee": "custom_app_api.permission_query_conditions.employee.get_permission_query_conditions",
 	"Route": "custom_app_api.permission_query_conditions.Route.get_permission_query_conditions",
 	"Point": "custom_app_api.permission_query_conditions.Point.get_permission_query_conditions",
+	"Area": "custom_app_api.permission_query_conditions.Area.get_permission_query_conditions",
+	"Zone": "custom_app_api.permission_query_conditions.Zone.get_permission_query_conditions"
 }
 #
 # has_permission = {
