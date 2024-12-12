389dcf7e8e43d4f5321b774dc1b132b0a1d94901
tusharmalpani20 <tusharmalpani20@gmail.com>
2024-12-12 12:57:04 +0530

    Added employee list to mark new joinee

--- custom_app_api/public/js/employee_list.js
+++ custom_app_api/public/js/employee_list.js
@@ -2,13 +2,14 @@ frappe.listview_settings["Employee"] = {
 	add_fields: ["status", "branch", "department", "designation", "image", "date_of_joining"],
 	filters: [["status", "=", "Active"]],
 	get_indicator: function (doc) {
-		// Check if employee joined in last 30 days
-		const thirtyDaysAgo = frappe.datetime.add_days(frappe.datetime.now_date(), -30);
-		const isNewEmployee = doc.date_of_joining >= thirtyDaysAgo;
+		// Only check for "New" status if employee is Active
+		if (doc.status === "Active") {
+			const thirtyDaysAgo = frappe.datetime.add_days(frappe.datetime.now_date(), -30);
+			const isNewEmployee = doc.date_of_joining >= thirtyDaysAgo;
 
-		// If employee is new, show "New" indicator, otherwise show regular status
-		if (isNewEmployee) {
-			return ["New", "blue", "date_of_joining,>=," + thirtyDaysAgo];
+			if (isNewEmployee) {
+				return ["New", "blue", "date_of_joining,>=," + thirtyDaysAgo];
+			}
 		}
 
 		var indicator = [__(doc.status), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];
