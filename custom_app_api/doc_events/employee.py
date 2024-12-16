5133fbcc9236c013a11f2a79018e707762837cf6
tusharmalpani20 <tusharmalpani20@gmail.com>
2024-12-16 17:37:14 +0530

    Added salary slip generation

--- custom_app_api/doc_events/employee.py
+++ custom_app_api/doc_events/employee.py
@@ -1,12 +1,7 @@
 import frappe
-import logging  # Import logging module
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)  # Set logging level
 
 def after_save(doc, method):
 
-    frappe.msgprint(f"Employee Separation created and submitted for {doc.employee_name}")
     if doc.custom_is_notice_period:
         # Create a new Employee Separation document
         separation_doc = frappe.get_doc({
@@ -22,9 +17,7 @@ def after_save(doc, method):
         # Submit the new document
         separation_doc.submit()
 
-        # Log the creation of the document
-        logging.info(f"Employee Separation document created for {doc.employee_name}")  # New logging line
 
         # Optionally, you can log or notify about the creation
-        frappe.msgprint(f"Employee Separation created and submitted for {doc.employee_name}")
+        # frappe.msgprint(f"Employee Separation created and submitted for {doc.employee_name}")
         frappe.msgprint("A new Employee Separation document has been created.")  # New msgprint line
