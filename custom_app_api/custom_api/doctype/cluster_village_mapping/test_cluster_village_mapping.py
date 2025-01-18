4a354f8cc94c7f0d8371a5790075b48008702da2
tusharmalpani20 <tusharmalpani20@gmail.com>
2025-01-18 14:37:01 +0530

    Added new doctypes and updated farmer_visit

--- custom_app_api/custom_api/doctype/cluster_village_mapping/test_cluster_village_mapping.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Hopnet and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestClusterVillageMapping(FrappeTestCase):
-	pass
