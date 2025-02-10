ab3a858d7ae28c501a55ec40cb4c946e32253da6
tusharmalpani20 <tusharmalpani20@gmail.com>
2025-02-10 14:28:17 +0530

    Added multiple mandals to bmc

--- custom_app_api/custom_api/doctype/bmc/bmc.js
+++ custom_app_api/custom_api/doctype/bmc/bmc.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, Hopnet and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("BMC", {
-// 	refresh(frm) {
+frappe.ui.form.on("BMC", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+
+	state: function(frm) {
+		// Clear mandal_list when state changes
+		frm.set_value('mandal_list', []);
+		
+		// Set filters for mandal_list based on selected state
+		frm.set_query('mandal_list', function() {
+			return {
+				filters: {
+					state: frm.doc.state
+				}
+			};
+		});
+	}
+});
