7053ed738e64bff3425c1c4bc45ca142ba71fdb0
tusharmalpani20 <tusharmalpani20@gmail.com>
2025-03-07 12:21:31 +0530

    Added a client script for employee promotion for promotion date

--- custom_app_api/public/js/employee_promotion.js
+++ custom_app_api/public/js/employee_promotion.js
@@ -3,24 +3,38 @@
 
 frappe.ui.form.on("Employee Promotion", {
 	setup: function(frm) {
+		console.log("Setup function called");
 		frm.set_query("promotion_date", function() {
+			console.log("set_query executed for promotion_date");
 			return {
 				filters: {
-					"promotion_date": [">=", frappe.datetime.get_today()]
 				}
 			};
 		});
 	},
 	
+	refresh: function(frm) {
+		console.log("Form refreshed - Script loaded successfully");
+		console.log("Current promotion date:", frm.doc.promotion_date);
+	},
+	
 	promotion_date: function(frm) {
+		console.log("Promotion date changed to:", frm.doc.promotion_date);
+		console.log("Today's date:", frappe.datetime.get_today());
+		
 		if (frm.doc.promotion_date < frappe.datetime.get_today()) {
+			console.log("Invalid date detected - showing message");
 			frappe.msgprint(__("Promotion date cannot be in the past. Please select today or a future date."));
 			frm.set_value("promotion_date", frappe.datetime.get_today());
 		}
 	},
 	
 	before_save: function(frm) {
+		console.log("Before save triggered");
+		console.log("Checking date:", frm.doc.promotion_date);
+		
 		if (frm.doc.promotion_date < frappe.datetime.get_today()) {
+			console.log("Invalid date detected - throwing error");
 			frappe.throw(__("Promotion date cannot be in the past. Please select today or a future date."));
 			return false;
 		}
