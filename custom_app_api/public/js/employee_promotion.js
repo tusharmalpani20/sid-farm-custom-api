95daad5c1ce4c099a806ce3649b845465f601f0e
tusharmalpani20 <tusharmalpani20@gmail.com>
2025-03-07 12:17:39 +0530

    Added a client script for employee promotion for promotion date

---
+++ custom_app_api/public/js/employee_promotion.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2024, Hopnet and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Employee Promotion", {
+	setup: function(frm) {
+		frm.set_query("promotion_date", function() {
+			return {
+				filters: {
+					"promotion_date": [">=", frappe.datetime.get_today()]
+				}
+			};
+		});
+	},
+	
+	promotion_date: function(frm) {
+		if (frm.doc.promotion_date < frappe.datetime.get_today()) {
+			frappe.msgprint(__("Promotion date cannot be in the past. Please select today or a future date."));
+			frm.set_value("promotion_date", frappe.datetime.get_today());
+		}
+	},
+	
+	before_save: function(frm) {
+		if (frm.doc.promotion_date < frappe.datetime.get_today()) {
+			frappe.throw(__("Promotion date cannot be in the past. Please select today or a future date."));
+			return false;
+		}
+	}
+});
